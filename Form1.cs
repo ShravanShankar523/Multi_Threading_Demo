08bafe178a4402e9f46a4d776b8e32a230c8f7e1
ShravanShankar523 <160073492+ShravanShankar523@users.noreply.github.com>
2024-04-03 21:39:36 -0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace Multi_Threading_Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<double> list = new List<double>();
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int count) && count > 0)
+            {
+                listBox1.Items.Clear();
+                await Task.Run(() =>
+                {
+                    CalculateFibonacci(count);
+
+                });
+                foreach (var item in list)
+                {
+                    listBox1.Items.Add(item);
+                }
+
+
+            }
+        }
+
+        private void CalculateFibonacci(int n)
+        {
+            Thread.Sleep(1000);
+            double a = 0;
+            double b = 1;
+            double c = 0;
+            for (int i = 0; i < n; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                list.Add(c);
+            }
+
+
+        }
+
+    }
+}
